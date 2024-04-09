b05774270d4301de51f5960929960a5cbc6fb437
Ryan Victor <ryaanvreis@gmail.com>
2024-04-09 09:02:04 -0300

    Atualização

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+    },
+    body: {
+        backgroundColor: '$gray900',
+        color: '$gray100',
+        '-webkit-font-smoothing': 'antialised',
+    },
+    'body, input, textare, button': {
+        fontFamily: 'Roboto',
+        fontWeight: '400'
+    },
+
+})
