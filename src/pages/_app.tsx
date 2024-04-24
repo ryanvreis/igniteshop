daca31fc77f320006cfc12157dff19b3adf3d54b
Ryan Victor <ryaanvreis@gmail.com>
2024-04-24 16:23:14 -0300

    feat: Adicionando novas funcionalidades

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,12 @@ import Image from "next/image";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
       </Header>
-
-        
+      
       <Component {...pageProps} />
     </Container>
     
