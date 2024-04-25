ff6a95443fc15d25f10bf64dae0c89974826c8d7
Ryan Victor <ryaanvreis@gmail.com>
2024-04-25 14:35:05 -0300

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,42 @@
 import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
-import logoImg from '@/assets/logo.svg'
+import logoImg from "@/assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
 import Image from "next/image";
+
+import Head from "next/head";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-      
-      <Component {...pageProps} />
-    </Container>
-    
-  ) 
+    <>
+      <Head>
+        {" "}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RHW2ET0CT3"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RHW2ET0CT3', 
+                page_path = window.location.pathname,
+              );
+            `,
+          }}
+        />
+      </Head>
+
+      <Container>
+        <Header>
+          <Image src={logoImg} alt="" />
+        </Header>
+
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
